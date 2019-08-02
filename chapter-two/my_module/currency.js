ace1af859d6e3ace774d4d90975347a54d4615f1
Robert Hieger <rhieger@gmail.com>
2019-08-02 02:03:19 -0400

    Chapter 2: Complete currency.js module.

---
+++ chapter-two/my_module/currency.js
@@ -0,0 +1,16 @@
+//  currency.js
+
+// Robert Hieger
+// August 1, 2019
+
+const canadianDollar  = 0.91;
+
+function roundTwo(amount)   {
+
+    return Math.round(amount * 100) /  100;
+
+}   // end roundTwo(amount)
+
+exports.canadianToUS = canadian =>rountTwo(canadian * canadianDollar);
+
+exports.USToCanadian = us =>  roundTwo(us / canadianDollar);
